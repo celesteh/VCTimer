22077ddd9cc98f8025e814a2e2f920e6c9672cf4
celesteh <celesteh@gmail.com>
2025-08-14 06:28:09 +0000

    First sketches

---
+++ src/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const { createServer } = require('node:http');
+const { join } = require('node:path');
+
+const app = express();
+const server = createServer(app);
+
+const dotenv = require('dotenv');
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.use(express.urlencoded({extended: 'false'}));
+app.use(express.json());
+
+const html_path = join(__dirname, "../public_html/");
+
+app.use(express.static(html_path)); // serve all the files
